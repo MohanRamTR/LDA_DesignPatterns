e053e75cd927398e993061eda30f6f55f99b9965
Mohan Ram T R <133201863+MohanRamTR@users.noreply.github.com>
2024-05-22 10:00:58 +0530

    commit

--- obj/Debug/net6.0/LDA_DesignPatterns_Chap1.AssemblyInfo.cs
+++ obj/Debug/net6.0/LDA_DesignPatterns_Chap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LDA_DesignPatterns_Chap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce367cd22a62b7cc5782cde979406954a7d86bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LDA_DesignPatterns_Chap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LDA_DesignPatterns_Chap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
